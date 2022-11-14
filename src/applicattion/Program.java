c2c8965bb2e7e7dd8c0af87240ceff9293152a52
Alexdevsoft <alexhavilla2022@gmail.com>
2022-11-13 23:20:41 -0300

    Primeiro commit

---
+++ src/applicattion/Program.java
@@ -0,0 +1,14 @@
+package applicattion;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getNumber();
+	}
+
+}
